6af0e4aec4d2cc57da39084b606cba8928241b06
Rayven <rayven.rey@gmail.com>
2021-12-27 14:38:08 +0100

    created models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_124226) do
+ActiveRecord::Schema.define(version: 2021_12_27_132942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.date "posted_date"
+    t.bigint "users_id", null: false
+    t.bigint "products_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["products_id"], name: "index_orders_on_products_id"
+    t.index ["users_id"], name: "index_orders_on_users_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +41,13 @@ ActiveRecord::Schema.define(version: 2021_12_27_124226) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "products", column: "products_id"
+  add_foreign_key "orders", "users", column: "users_id"
 end

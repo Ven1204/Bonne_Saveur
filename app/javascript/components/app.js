43390be1c5d0471ac9a287a45565532905c2bd26
Rayven <rayven.rey@gmail.com>
2021-12-27 22:40:29 +0100

    fonts and navbar added

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,9 +1,14 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import classes from './app.module.css';
 
 class App extends Component {
   render() {
     return (
+      <div className={classes.container}>
       <h1>Welcome to Bonne Saveur!</h1>
+      <h2>Coming Soon!!</h2>
+      </div>
+
     )
   }
 };

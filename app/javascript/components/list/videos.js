df1b13933a4451274ac986cae32955aba4250576
Rayven <rayven.rey@gmail.com>
2022-01-05 22:41:24 +0100

    practice button hide show component

--- app/javascript/components/list/videos.js
+++ app/javascript/components/list/videos.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='pt-4 pb-4'>
-      <iframe width="100%" height="1000%" src="https://gifer.com/embed/8Nuh" frameborder="0"></iframe>
+      <iframe width="100%" height="100%" src="https://gifer.com/embed/8Nuh" frameborder="0"></iframe>
     </div>
   )
 }

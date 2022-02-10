d07ca4a674956147e8967cff722e84b1e05716d6
Rayven <rayven.rey@gmail.com>
2022-02-10 17:29:41 +0100

    added active list and list component

--- app/javascript/components/list/activeList.jsx
+++ app/javascript/components/list/activeList.jsx
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 
-export class ActiveList extends Component{
+export default class ActiveList extends Component{
+  state = {
+
+  }
+
   render(){
     return(
+      <div>
+        <h2>this will return clickable cards of products with name and image </h2>
+      </div>
 
-      <h1>
-        asdsfghkjfgsdhsdgjkhgf
-      </h1>
-        // <div className="col col-sm-12">
-        //   <h1>Our Products</h1>
-
-        //   <div style={{backgroundColor: 'blue'}}>
-        //     <h1>Cakes</h1>
-        //     <h2>asdgjhkjhsafhf</h2>
-        //   </div>
-        //   <div style={{backgroundColor: 'green'}}>
-        //     <h1>Candies</h1>
-        //   </div>
-        //   <div style={{backgroundColor: 'orange'}}>
-        //     <h1>Breads</h1>
-        //   </div>
-        //   <div style={{backgroundColor: 'red'}}>
-        //     <h1>Others</h1>
-        //   </div>
-        // </div>
       );
   }
 }
--- app/javascript/components/list/list.jsx
+++ app/javascript/components/list/list.jsx
@@ -1,33 +1,46 @@
 import React,  { Component } from 'react';
-import activeList from './activeList';
+import ActiveList from './activeList';
 
 
 class List extends Component {
 
-  render(){
+  state  = {
 
-  const handleClick = () => {
-    <activeList />
+  }
+  // event when clicked the divs will  show the active list on the right
+  handleClick = () => {
+    <ActiveList />
   }
 
-  return(
-    <div className="col col-sm-12">
-      <h1>Our Products</h1>
-
-      <div onClick={this.handleClick} style={{backgroundColor: 'blue'}}>
-        <h1>Cakes</h1>
+  render(){
+    return(
+      <div >
+        {/* div for the menu of products */}
+        <h1>Our Products</h1>
+        <div className='row'>
+          <div className='col-4' style={{cursor: 'pointer'}}>
+            <div>
+              <div onClick={this.handleClick} style={{backgroundColor: 'blue'}}>
+                <h1>Cakes</h1>
+              </div>
+              <div onClick={this.handleClick} style={{backgroundColor: 'green'}}>
+                <h1>Candies</h1>
+              </div>
+              <div onClick={this.handleClick} style={{backgroundColor: 'orange'}}>
+                <h1>Breads</h1>
+              </div>
+              <div onClick={this.handleClick} style={{backgroundColor: 'red'}}>
+                <h1>Others</h1>
+              </div>
+            </div>
+          </div>
+        {/* div for the cards  */}
+          <div className="col-8">
+            {/* <ActiveList /> */}
+          </div>
+        </div>
       </div>
-      <div onClick={handleClick} style={{backgroundColor: 'green'}}>
-        <h1>Candies</h1>
-      </div>
-      <div onClick={handleClick} style={{backgroundColor: 'orange'}}>
-        <h1>Breads</h1>
-      </div>
-      <div onClick={handleClick} style={{backgroundColor: 'red'}}>
-        <h1>Others</h1>
-      </div>
-    </div>
-  );
+    );
   }
 }
 
